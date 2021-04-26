317f72666849ce060352706dc3ab69aae115faec
Enrique Ibarra <rique.ibarra1@gmail.com>
2021-04-26 09:51:01 -0500

    added sign in component, ready for firebase

---
+++ src/components/signin/sign-in.component.jsx
@@ -0,0 +1,40 @@
+import React, {Component} from 'react';
+import './sign-in.styles.scss';
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../button/custom-button.component';
+class SignIn extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+        this.setState({email: '', password: ''})
+    };
+    handleChange = (e) => {
+        const {value, name} = e.target;
+        //[name] here is the name of the key in our state object, value is our form submitted value.
+        //will set email: value/password: value
+        this.setState({[name] : value})
+    }
+    render() {
+        return (
+            <div className="sign-in">
+                <h2>I already have an account</h2>
+                <span>Sign in with your email and password</span>
+                <form onSubmit={this.handleSubmit}>
+                    <FormInput name="email" type="Email" value={this.state.email} handleChange={this.handleChange} label="email" required />
+                    <FormInput name="password" type="Password" value={this.state.password} handleChange={this.handleChange} label="password" required />
+                    <CustomButton type="submit" value="Submit Form">SIGN IN</CustomButton>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SignIn;
---
+++ src/pages/sign-in-sign-up/sign-in-sign-up.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './sign-in-sign-up.styles.scss';
+import SignIn from '../../components/signin/sign-in.component';
+const SignInAndSignUpPage = () => {
+    return (
+        <div className="sign-in-and-sign-up-page">
+            <SignIn />
+        </div>
+    )
+}
+
+export default SignInAndSignUpPage;
