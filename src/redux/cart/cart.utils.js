ad74a21ec62ac6a485780a0208089ad9522ace4d
Enrique Ibarra <rique.ibarra1@gmail.com>
2021-04-27 18:34:36 -0500

    additem and remove item functionality in cart

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -9,5 +9,17 @@ export const addItemToCart = (cartItems, newItem) => {
             : cartItem);
     }
     return [...cartItems, { ...newItem, quantity: 1 }];
-
+}
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+    const existingCartItem = cartItems.find(
+        cartItem => cartItem.id === cartItemToRemove.id
+    )
+    if(existingCartItem.quantity === 1) {
+        return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id)
+    }
+    //return the cartItems, if there is a matching ID lower its quantity, otherwise return
+    return cartItems.map(
+        cartItem => cartItem.id === cartItemToRemove.id ? {...cartItem, quantity: cartItem.quantity -1 }
+        : cartItem
+    )
 }
