0dd8418ece476571c140ba9165c51b6b5920618c
Enrique Ibarra <rique.ibarra1@gmail.com>
2021-04-27 19:06:46 -0500

    added session handling with redux-persist, stored cart in session data

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,18 @@
 import {combineReducers} from 'redux';
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
+import {persistReducer} from 'redux-persist';
+//the local storage on our window object, window.localstorage
+import storage from 'redux-persist/lib/storage';
 
-export default combineReducers({
+const persistConfig = {
+    key: 'root',
+    storage,
+    //lets redux resist know the only data we want to persist is the cart state
+    whitelist: ['cart']
+}
+const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer,
 });
+export default persistReducer(persistConfig, rootReducer);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,12 @@
 import {createStore, applyMiddleware} from 'redux';
 import logger from 'redux-logger';
 import rootReducer from './root-reducer';
+import { persistStore } from 'redux-persist';
 
 const middlewares = [logger];
 //applyMiddleware can take in any amount of middleware
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
+//a session-persisted version of our store
+const persistor = persistStore(store);
 
-export default store;
+export {store, persistor};
