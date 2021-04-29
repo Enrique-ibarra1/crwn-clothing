a366ea368cb43bfea1c1aa0936619d1bced59ded
Enrique Ibarra <rique.ibarra1@gmail.com>
2021-04-29 09:16:51 -0500

    fixes for deployment

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -5,7 +5,7 @@ const StripeCheckoutButton = ({price}) => {
     const priceForStripe = price * 100;
     const API_KEY = process.env.REACT_APP_API_KEY;
     const publishableKey = API_KEY;
-    const ENVKEY = '';
+    // const ENVKEY = '';
 
     const onToken = token => {
         console.log(token);
--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,12 @@ import logger from 'redux-logger';
 import rootReducer from './root-reducer';
 import { persistStore } from 'redux-persist';
 
-const middlewares = [logger];
+//logger will only be availaable in production
+const middlewares = [];
+//there is an env variable in node, which will change when deployed to production
+if (process.env.NODE_ENV === 'development') {
+    middlewares.push(logger);
+}
 //applyMiddleware can take in any amount of middleware
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
 //a session-persisted version of our store
