0ba5efcec29c0b6a5c9050f535bd3d71635638f7
Enrique Ibarra <rique.ibarra1@gmail.com>
2021-04-29 09:21:39 -0500

    fixes for deployment 2

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -6,7 +6,7 @@ import {selectCollection} from '../../redux/shop/shop.selectors';
 
 const CollectionPage = ({collection}) => {
     const {title, items } = collection
-    console.log(collection)
+
     return (
         <div className="collection-page">
             <h2 className="title">{title}</h2>
