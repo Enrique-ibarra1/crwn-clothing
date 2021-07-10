84208d1e621de2c77033f1303d549cc1e1a9b6c7
Enrique Ibarra <rique.ibarra1@gmail.com>
2021-07-10 13:55:06 -0500

    moved cart logic into sagas, cart is now cleared on signout

--- src/redux/root-saga.js
+++ src/redux/root-saga.js
@@ -1,11 +1,13 @@
 import {all, call} from 'redux-saga/effects';
 import {fetchCollectionsStart} from './shop/shop.sagas';
 import {userSagas} from './user/user-sagas';
+import { cartSagas } from './cart/cart.sagas';
 //all takes in an array of sagas
 export default function* rootSaga() {
     yield all([
         call(fetchCollectionsStart),
-        call(userSagas)
+        call(userSagas),
+        call(cartSagas)
         ]
     )
 }
