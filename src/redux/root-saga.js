394a63ba0feeeb65f1c5859520d0ec13e87693b3
Enrique Ibarra <rique.ibarra1@gmail.com>
2021-07-10 14:57:02 -0500

    moved sign up logic to saga

--- src/redux/root-saga.js
+++ src/redux/root-saga.js
@@ -1,5 +1,5 @@
 import {all, call} from 'redux-saga/effects';
-import {fetchCollectionsStart} from './shop/shop.sagas';
+import {fetchCollectionsStart, shopSagas} from './shop/shop.sagas';
 import {userSagas} from './user/user-sagas';
 import { cartSagas } from './cart/cart.sagas';
 //all takes in an array of sagas
@@ -7,7 +7,8 @@ export default function* rootSaga() {
     yield all([
         call(fetchCollectionsStart),
         call(userSagas),
-        call(cartSagas)
+        call(cartSagas),
+        call(shopSagas)
         ]
     )
 }
--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,4 +1,4 @@
-import { takeLatest, call, put } from 'redux-saga/effects';
+import { takeLatest, call, put, all } from 'redux-saga/effects';
 import ShopActionTypes from './shop.types';
 import {firestore, collectionsSnapshotToMap} from '../../firebase/firebase.utils';
 import {fetchCollectionsSuccess, fetchCollectionsFailure} from './shop.actions'
@@ -24,4 +24,8 @@ export function* fetchCollectionsAsync() {
 export function* fetchCollectionsStart() {
     //the second parameter is another generator function
     yield takeLatest(ShopActionTypes.FETCH_COLLECTIONS_START, fetchCollectionsAsync )
+}
+
+export function* shopSagas() {
+    yield all([call(fetchCollectionsStart)])
 }
